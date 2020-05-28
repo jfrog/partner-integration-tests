e8fc8b65fef785e10b2f8a97d6d932afa886f64d
danielmkn <danielmi@jfrog.com>
2020-05-28 13:01:05 -0700

    Initial commit. Testing framework has been moved from Fozzie repo to it's separate repo

---
+++ README.md
@@ -0,0 +1,45 @@
+## Partnership Engineering Integration Testing Framework
+
+The purpose of this framework is to test different Artifactory configurations deployed by various Partnership Engineering solutions.
+It can be used in the CI-CD process and in a single manual test run.
+
+Test suites include Artifactory Pro, JCR and X-ray tests.   
+
+### How to run it locally
+Clone the repo. Open the file ```/src/test/resources/testenv.yaml``` and fill it with your environment values. Mandatory fields: 
+
+`external_ip` - load balancer or Artifactory node IP address
+
+`rt_username` - Artifactory username (`admin` by default)
+
+`rt_password` - Artifactory password (`password` by default)
+
+`url` fields is used in Docker tests only and must be valid DNS name
+
+Run Gradle wrapper to invoke Gradle task: 
+```
+./gradlew <task_name>
+```
+
+### Available tasks
+```
+artifactory_jcr_test
+artifactory_ha_test
+unified_test
+artifactory_ha_docker_test
+artifactory_jcr_docker_test
+```
+Docker tests require SSL setup which is not always needed or available.
+For this reason they are moved to the separate tasks. 
+
+### Adding new tests
+We use Groovy and REST-Assured in our tests. TestNG is being use as a test management tool. 
+New tests could be added in `/src/test/groovy/tests`
+All tests should use steps, which is a collection of Artifactory API calls. 
+Each new test class should be added to `testing.xml` file and should be assigned to at least one group - ```groups=["common","jcr","pro","docker"]```
+
+### Gradle cleanup. Delete the folder:
+```
+ ~/.gradle/caches/
+ ./gradlew clean
+```
---
+++ src/test/groovy/steps/RepositorySteps.groovy
@@ -0,0 +1,170 @@
+package steps
+
+
+import static io.restassured.RestAssured.given
+
+class RepositorySteps {
+
+    def getHealthCheckResponse(artifactoryURL) {
+        return given()
+                .when()
+                .get("http://" + artifactoryURL + "/router/api/v1/system/health")
+                .then()
+                .extract().response()
+    }
+
+    def ping() {
+        return given()
+                .when()
+                .get("/api/system/ping")
+                .then()
+                .extract().response()
+    }
+
+    def createRepositories(File body, username, password) {
+        return given()
+                .auth()
+                .preemptive()
+                .basic("${username}", "${password}")
+                .header("Cache-Control", "no-cache")
+                .header("content-Type", "application/yaml")
+                .body(body)
+                .when()
+                .patch("/api/system/configuration")
+                .then()
+                .extract().response()
+    }
+    // https://www.jfrog.com/confluence/display/JFROG/Artifactory+REST+API#ArtifactoryRESTAPI-GetRepositories
+    def getRepos() {
+        return given()
+                .header("Cache-Control", "no-cache")
+                .header("content-Type", "application/yaml")
+                .when()
+                .get("/api/repositories")
+                .then()
+                .extract().response()
+
+    }
+    // https://www.jfrog.com/confluence/display/JFROG/Artifactory+REST+API#ArtifactoryRESTAPI-DeleteRepository
+    def deleteRepository(repoName, username, password) {
+        return given()
+                .auth()
+                .preemptive()
+                .basic("${username}", "${password}")
+                .header("Cache-Control", "no-cache")
+                .header("content-Type", "application/yaml")
+                .when()
+                .delete("/api/repositories/" + repoName)
+                .then()
+                .extract().response()
+
+    }
+
+    def createDirectory(repoName, directoryName) {
+        return given()
+                .header("Cache-Control", "no-cache")
+                .header("content-Type", "application/yaml")
+                .when()
+                .put("/" + repoName + "/" + directoryName)
+                .then()
+                .extract().response()
+
+    }
+
+    def deployArtifact(repoName, directoryName, artifact, filename, sha256, sha1, md5) {
+        return given()
+                .header("Cache-Control", "no-cache")
+                .header("Content-Type", "application/json")
+                .header("X-Checksum-Sha256", sha256)
+                .header("X-Checksum-Deploy", "false")
+                .header("X-Checksum-Sha1", sha1)
+                .header("X-Checksum", md5)
+                .body(artifact)
+                .when()
+                .put("/" + repoName + "/" + directoryName + "/" + filename)
+                .then()
+                .extract().response()
+
+    }
+
+    def addChecksumToArtifact(repoName, directoryName, filename) {
+        return given()
+                .header("Cache-Control", "no-cache")
+                .header("Content-Type", "application/json")
+                .body("{\n" +
+                        "   \"repoKey\":\"${repoName}\",\n" +
+                        "   \"path\":\"${directoryName}/${filename}\"\n" +
+                        "}")
+                .when()
+                .post("/api/checksum/sha256")
+                .then()
+                .extract().response()
+
+    }
+
+    def deleteItem(path) {
+        return given()
+                .header("Cache-Control", "no-cache")
+                .header("Content-Type", "application/json")
+                .when()
+                .delete("/" + path)
+                .then()
+                .extract().response()
+
+    }
+
+    def getInfo(path) {
+        return given()
+                .header("Cache-Control", "no-cache")
+                .header("Content-Type", "application/json")
+                .when()
+                .get("/api/storage/" + path)
+                .then()
+                .extract().response()
+
+    }
+
+    def listDockerTags(username, password, repoKey, imageName, listSize, endTag) {
+        return given()
+                .auth()
+                .preemptive()
+                .basic("${username}", "${password}")
+                .header("Cache-Control", "no-cache")
+                .header("Content-Type", "application/json")
+                .when()
+                .get("/api/docker/${repoKey}/v2/${imageName}/tags/list?n=${listSize}&last=${endTag}")
+                .then()
+                .extract().response()
+
+    }
+
+    def createSupportBundle(name, startDate, endDate) {
+        return given()
+                .header("Cache-Control", "no-cache")
+                .header("Content-Type", "application/json")
+                .body("{ \n" +
+                        "   \"name\":\"${name}\",\n" +
+                        "   \"description\":\"desc\",\n" +
+                        "   \"parameters\":{ \n" +
+                        "      \"configuration\": \"true\",\n" +
+                        "      \"system\": \"true\",             \n" +
+                        "      \"logs\":{ \n" +
+                        "         \"include\": \"true\",          \n" +
+                        "         \"start_date\":\"${startDate}\",\n" +
+                        "         \"end_date\":\"${endDate}\"\n" +
+                        "      },\n" +
+                        "      \"thread_dump\":{ \n" +
+                        "         \"count\": 1,\n" +
+                        "         \"interval\": 0\n" +
+                        "      }\n" +
+                        "   }\n" +
+                        "}")
+                .when()
+                .post("/api/system/support/bundle")
+                .then()
+                .extract().response()
+
+    }
+
+
+}
