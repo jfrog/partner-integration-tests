7d1f49684961928ff0606691ae55614c6874bc7e
danielmkn <danielmi@jfrog.com>
2020-12-15 11:58:19 -0800

    Artifactory, Apply single license script added

--- scripts/apply_single_license.sh
+++ scripts/apply_single_license.sh
@@ -9,22 +9,12 @@ RT_USERNAME=$1
 RT_PASSWORD=$2
 IP_ADDR=$3
 LICENSE1=$4
-LICENSE2=$5
-LICENSE3=$6
 
 echo "### Add licenses ###"
 curl -X POST -u ${RT_USERNAME}:${RT_PASSWORD} \
   http://${IP_ADDR}/artifactory/api/system/licenses \
   -H 'Cache-Control: no-cache' \
   -H 'Content-Type: application/json' \
-  -d '[
-        {
+  -d '{
           "licenseKey": "'${LICENSE1}'"
-        },
-        {
-          "licenseKey": "'${LICENSE2}'"
-        },
-        {
-          "licenseKey": "'${LICENSE3}'"
-        }
-      ]'
+       }'
