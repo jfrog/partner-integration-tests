a2b2b07ad73d7bb7d98d18f4261c6cab2b7d1755
danielmkn <danielmi@jfrog.com>
2020-12-28 16:00:30 -0800

    test

--- scripts/accept_eula.sh
+++ scripts/accept_eula.sh
@@ -7,6 +7,11 @@ RT_USERNAME=$1
 RT_PASSWORD=$2
 IP_ADDR=$3
 
+echo "1 "${RT_USERNAME}
+echo "2 "${RT_PASSWORD}
+echo "3 "${IP_ADDR} "3 " $3
+
+
 echo "### Accept EULA for JCR distrubution on ${IP_ADDR}###"
 OUT=$(curl -XPOST -vu ${RT_USERNAME}:${RT_PASSWORD} http://${IP_ADDR}/artifactory/ui/jcr/eula/accept 2>&1)
     echo "$OUT"
