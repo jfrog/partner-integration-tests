62f15457982618b03802cbb056255eb48472821b
Daniel Miakotkin <55418063+danielmkn@users.noreply.github.com>
2021-04-02 10:53:07 -0700

    Update apply_single_license.sh
    Make 'post license' call silent to hide the actual license from the output. Stderr will still be visible

--- scripts/apply_single_license.sh
+++ scripts/apply_single_license.sh
@@ -11,10 +11,11 @@ IP_ADDR=$3
 LICENSE1=$4
 
 echo "### Add licenses ###"
-curl -X POST -u ${RT_USERNAME}:${RT_PASSWORD} \
+curl -s -S -X POST -u ${RT_USERNAME}:${RT_PASSWORD} \
   http://${IP_ADDR}/artifactory/api/system/licenses \
   -H 'Cache-Control: no-cache' \
   -H 'Content-Type: application/json' \
   -d '{
           "licenseKey": "'${LICENSE1}'"
-       }'
+       }' \
+  --fail > /dev/null
