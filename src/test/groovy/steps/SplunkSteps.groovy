ae5c5eb11a5df70567c326435f3dd132ef966fd8
danielmkn <danielmi@jfrog.com>
2021-04-14 17:55:40 -0700

    PTRENG-1707, add tests for new Docker dashboard widgets. IP address verification method changed. Some refactoring has been done.

--- src/test/groovy/steps/SplunkSteps.groovy
+++ src/test/groovy/steps/SplunkSteps.groovy
@@ -1,7 +1,11 @@
 package steps
 
 import io.restassured.response.Response
+import org.apache.commons.validator.routines.InetAddressValidator
+import org.awaitility.Awaitility
 import org.yaml.snakeyaml.Yaml
+
+import java.util.concurrent.TimeUnit
 
 import static io.restassured.RestAssured.given
 
@@ -10,10 +14,8 @@ class SplunkSteps {
     Yaml yaml = new Yaml()
     def configFile = new File("./src/test/resources/testenv.yaml")
     def config = yaml.load(configFile.text)
-    def url = "http://${config.artifactory.external_ip}/xray/api"
     def distribution = config.artifactory.distribution
-    def username = config.artifactory.rt_username
-    def password = config.artifactory.rt_password
+    def repoSteps = new RepositorySteps()
 
     // Splunk API documentation
     // https://docs.splunk.com/Documentation/Splunk/8.0.5/RESTREF/RESTsearch#search.2Fjobs
@@ -26,7 +28,6 @@ class SplunkSteps {
                 .header("content-Type", "application/x-www-form-urlencoded")
                 .body(search_string)
                 .when()
-                .log().everything()
                 .post(splunk_url + "/services/search/jobs")
                 .then()
                 .extract().response()
@@ -53,6 +54,37 @@ class SplunkSteps {
         def searchID = createSearch.then().extract().path("sid")
         println "Search ID is " + searchID
         return searchID
+    }
+
+    def waitForTheResponse(splunk_username, splunk_password, splunkBaseURL, searchID, secondsToWait){
+        Awaitility.await().atMost(secondsToWait, TimeUnit.SECONDS).until(() ->
+                (getSearchResults(splunk_username, splunk_password, splunkBaseURL, searchID)).then().
+                        extract().statusCode() == 200)
+    }
+
+    def dockerCleanupCache(artifactoryBaseURL, username, password, repoPath, images){
+        for(imageName in images) {
+            def path = repoPath + imageName
+            Response deleteDockerRepos = repoSteps.deleteItem(artifactoryBaseURL, username, password,
+                    path)
+            deleteDockerRepos.then().log().ifValidationFails().statusCode(204)
+        }
+    }
+
+    def splunkSearchResults(splunk_username, splunk_password, splunkBaseURL, search_string){
+        def searchID = getSplunkSearchID(splunk_username, splunk_password, splunkBaseURL, search_string)
+        waitForTheResponse(splunk_username, splunk_password, splunkBaseURL, searchID, 120)
+        return getSearchResults(splunk_username, splunk_password, splunkBaseURL, searchID)
+    }
+
+    def validateIPAddress(IPAddress){
+        InetAddressValidator validator = InetAddressValidator.getInstance()
+        if (validator.isValid(IPAddress)) {
+            return true
+        }
+        else {
+            return false
+        }
     }
 
 }
